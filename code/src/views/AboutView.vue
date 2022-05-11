e4b5d17dff6f3eb1033f2de241bbc0ff85f2d278
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-11 19:10:38 +0200

    redoing

---
+++ code/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<script>
+  export default 
+  {
+    name: 'AboutView',
+    components: 
+    {
+      
+    }
+  }
+</script>
+<template>
+  <div class="view-about">
+    
+  </div>
+</template>
