a6ea9d1c2b5b98453d36f79121bbcc88266562a0
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-11 19:13:33 +0200

    forward

--- code/src/router/index.js
+++ code/src/router/index.js
@@ -17,7 +17,7 @@ const routes =
   {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   },
 ]
 
