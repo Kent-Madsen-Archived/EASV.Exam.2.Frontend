099857b46a3920f1a6145410265fa4c5759504f7
kent vejrup madsen <kent.vejrup.madsen@protonmail.com>
2022-05-11 19:07:47 +0200

    current

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,28 +1,21 @@
 <script>
-  import FooterAreaComponent 
-    from "./components/Areas/FooterAreaComponent.vue";
-
-  import HeaderAreaComponent 
-    from "./components/Areas/HeaderAreaComponent.vue";
-
 
   export default 
   {
     name: "Application",
     components: 
     {
-    FooterAreaComponent,
-    HeaderAreaComponent
-}
+      
+    }
   }
 </script>
 <style scoped>
-  @import "style.css";
+  @import "./assets/css/application.css";
 </style>
 <template>
-  <HeaderAreaComponent/>
+
   <main>
     <router-view/>
   </main>
-  <FooterAreaComponent/>
+  
 </template>
--- code/src/store/index.js
+++ code/src/store/index.js
@@ -1,44 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } 
+  from 'vuex'
 
 export default createStore(
   {
     state: 
     {
-      user:
-      {
-        username: null,
-        bearer_token: null,
-      }
+
     },
     getters: 
     {
-      RetrieveAccountName( state )
-      {
-        return state.user.username;
-      },
 
-      RetrieveBearerToken( state )
-      {
-        return state.user.bearer_token;
-      },
-
-      RetrieveUserState( state )
-      {
-        return !(state.user.username === null) && !(state.user.bearer_token === null);
-      }
-      
     },
     mutations: 
     {
-      SetAccountName(state, data)
-      {
-        state.user.username = data;
-      },
-
-      SetAccountBearerToken(state, data)
-      {
-        state.user.bearer_token = data;
-      }
+      
     },
     methods:
     {
@@ -46,9 +21,11 @@ export default createStore(
     },
     actions: 
     {
+
     },
     modules: 
     {
+
     }
   }
 )
